515855a90dd8663c97c1eb32ba17e58224a3b185
Дубовик Максим <lufton@gmail.com>
2017-01-25 00:41:32 +0200

    Changed data

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
  */
 jQuery(function($) {
     $.getJSON("https://rawgit.com/lufton/ChristianRadio/master/data.json", function(data) {
-
+        var audio = new Audio(data.stations[0].streams[0].url);
+        audio.play();
     });
 });
