40c9768d55fc7d8ce6b1959b5450c5e385c58711
Дубовик Максим <lufton@gmail.com>
2017-01-26 01:18:00 +0200

    Some test changes

--- background.js
+++ background.js
@@ -3,6 +3,7 @@
  */
 jQuery(function($) {
     var audio = new Audio;
+    $.ajaxSetup({ cache: false });
     $.getJSON("https://rawgit.com/lufton/ChristianRadio/master/data.json", function(data) {
         window.data = data;
     });
@@ -15,5 +16,16 @@ jQuery(function($) {
     window.pause = function () {
         audio.pause();
         audio.src = '';
-    }
+    };
+
+    var iconFrame = 0;
+    var frameCount = 6;
+    setInterval(function() {
+        if (audio.paused) chrome.browserAction.setIcon({path : "icons/icon0.png"});
+        else chrome.browserAction.setIcon({path : "icons/icon" + iconFrame++ % frameCount + ".png"});
+    }, 250);
+
+    var updateIcon = function() {
+
+    };
 });
