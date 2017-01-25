d684b19ccea9be3344f2d7fface0c7e2a331de64
Дубовик Максим <lufton@gmail.com>
2017-01-25 13:26:16 +0200

    Changed data

--- popup.js
+++ popup.js
@@ -6,16 +6,30 @@ jQuery(function($) {
     $('#stations').isotope({
         
     });
+    $('#genres').on('change', 'input', function() {
+        backPage.currentGenre = this.value;
+    });
     var populateGenres = function() {
-        var currentGenre = $('#genres input:checked').val();
+        var currentGenre = $('#genres input:checked').val() || backPage.currentGenre;
         $('#genres label:nth-child(n+2)').remove();
 
-        if (backPage.data && backPage.data.stations) {
-            $.each(backPage.data.stations, function (i, station) {
-                
+        if (backPage && backPage.data && backPage.data.genres) {
+            $.each(backPage.data.genres, function (genre, title) {
+                $('#genres').append('<label class="btn btn-default"><input type="radio" name="genre" value="' + genre + '">' + title +'</label>');
             });
         }
         $('#genres input[value="*"]').click();
         $('#genres input[value="' + currentGenre + '"]').click();
     };
+    var populateStations = function() {
+        $('#stations li').remove();
+        if (backPage && backPage.data && backPage.data.stations) {
+            $.each(backPage.data.stations, function() {
+                
+            })
+        }
+    };
+
+    populateGenres();
+    populateStations();
 });
