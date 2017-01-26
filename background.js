d7c14747d57546e8083e9a3e7e8d1edf8129b115
Дубовик Максим <lufton@gmail.com>
2017-01-26 22:40:36 +0200

    Some test changes

--- background.js
+++ background.js
@@ -3,13 +3,16 @@
  */
 jQuery(function($) {
     var audio = new Audio;
+    window.paused = function () {
+        return audio.paused;
+    };
     $.ajaxSetup({ cache: false });
     $.getJSON("https://rawgit.com/lufton/ChristianRadio/master/data.json", function(data) {
         window.data = data;
     });
 
-    window.play = function () {
-        audio.src = window.data.stations[window.currentStation].streams[0].url;
+    window.play = function (stream) {
+        audio.src = stream || window.data.stations[window.currentStation].streams[0].url;
         audio.play();
     };
 
@@ -21,11 +24,7 @@ jQuery(function($) {
     var iconFrame = 0;
     var frameCount = 6;
     setInterval(function() {
-        if (audio.paused) chrome.browserAction.setIcon({path : "icons/icon0.png"});
+        if (window.paused()) chrome.browserAction.setIcon({path : "icons/icon0.png"});
         else chrome.browserAction.setIcon({path : "icons/icon" + iconFrame++ % frameCount + ".png"});
     }, 250);
-
-    var updateIcon = function() {
-
-    };
 });
--- popup.js
+++ popup.js
@@ -15,11 +15,16 @@ jQuery(function($) {
         backPage.currentLanguage = this.value;
         $('#stations').isotope({filter: this.value});
     });
-    $('#stations').on('click', '.station .play', function() {
-        backPage.currentStation = this.parentElement.id;
-        if ($(this.parentElement).hasClass('active')) backPage.pause(); else backPage.play();
+    $('#stations').on('click', '.station', function() {
+        $(this).toggleClass('expanded');
+        $(this).parent().toggleClass('expanded');
+    });
+    $('#stations').on('click', 'a.stream', function() {
+        backPage.currentStation = $(this).data('station');
+        if ($(this).hasClass('playpause') && !backPage.paused()) backPage.pause(); else backPage.play($(this).attr('src'));
         $('.station:not(#' + backPage.currentStation + ')').removeClass('active');
-        $(this.parentElement).toggleClass('active');
+        if ($(this).hasClass('playpause')) $(this).closest('.station').toggleClass('active'); else $(this).closest('.station').addClass('active');
+        return false;
     });
     var populateGenres = function() {
         var currentGenre = $('#genres input:checked').val() || backPage.currentGenre;
@@ -33,17 +38,48 @@ jQuery(function($) {
         $('#genres input[value="*"]').click();
         $('#genres input[value="' + currentGenre + '"]').click();
     };
+    var populateLanguages = function() {
+        var currentLanguage = $('#languages input:checked').val() || backPage.currentLanguage;
+        $('#languages label:nth-child(n+2)').remove();
+
+        if (backPage && backPage.data && backPage.data.languages) {
+            $.each(backPage.data.languages, function (language, title) {
+                $('#languages').append('<label class="btn btn-default"><input type="radio" name="language" value=".lang-' + language + '">' + title +'</label>');
+            });
+        }
+        $('#genres input[value="*"]').click();
+        $('#genres input[value="' + currentGenre + '"]').click();
+    };
     var populateStations = function() {
         //$('#stations li').remove();
         if (backPage && backPage.data && backPage.data.stations) {
             $.each(backPage.data.stations, function(id, station) {
                 $station = $('<li id="' + id + '" class="station ' +
-                    (id==backPage.currentStation?'active ':'') +
+                    (id==backPage.currentStation && !backPage.paused()?'active ':'') +
                     $.map(station.genres, function(genre) { return 'genre-' + genre; }).join(' ') + ' ' +
                     $.map(station.languages, function(language) { return 'lang-' + language; }).join(' ') + '">' +
-                    '<img src="icons/' + id + '.png" class="img-rounded" />' +
-                    '<span>' + station.title + '</span>' +
-                    '<div class="play"></div>' +
+                    '<aside><img src="icons/' + id + '.png" class="img-rounded" />' +
+                    '<div class="metadata">' +
+                        '<div class="form-group">' +
+                            '<label>Потоки (kbps)</label>' +
+                            '<div class="clear"></div>' +
+                            '<div class="btn-group btn-group-xs" data-toggle="buttons">' + $.map(station.streams, function(stream, i) { return '<a class="btn btn-' + (i>0?'default':'primary') + ' stream" src="' + stream.url + '" data-station="' + id + '">' + stream.bitrate + '</a>'; }).join('') + '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<label>Жанры</label>' +
+                            '<div class="clear"></div>' +
+                            '<div class="btn-group btn-group-xs" data-toggle="buttons">' + $.map(station.genres, function(genre) { return '<button class="btn btn-default"  data-filter=".genre-' + genre + '">' + genre + '</button>'; }).join('') + '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<label>Языки</label>' +
+                            '<div class="clear"></div>' +
+                            '<div class="btn-group btn-group-xs" data-toggle="buttons">' + $.map(station.languages, function(language) { return '<button class="btn btn-default"  data-filter=".lang-' + language + '">' + backPage.data.languages[language] + '</button>'; }).join('') + '</div>' +
+                        '</div>' +
+                    '</div>' +
+                    '</aside>' +
+                    '<h1>' + station.title + '</h1>' +
+                    '<a class="play playpause stream" data-station="' + id + '"></a>' +
+                    '<div class="description">' + station.description + '</div>' +
                     '</li>');
                 $('#stations').append($station).isotope('appended', $station);
             });
@@ -51,5 +87,6 @@ jQuery(function($) {
     };
 
     populateGenres();
+    populateLanguages();
     populateStations();
 });
